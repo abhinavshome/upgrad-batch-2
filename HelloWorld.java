af3440dfd032d5a5052f64287dd2279dcaab87c6
Kumar Abhinav <kabhinav.iitg@gmail.com>
2025-12-01 16:41:07 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("How are you today?");
+
+
+        byte smallNumber = -123;
+        short anotherNumber = 1223;
+        long n = 23;
+
+
+        double distance = 12.22;
+        float heightInCms = 180.22f;
+
+        System.out.println(n);
+        System.out.println(heightInCms);
+    }
+}
